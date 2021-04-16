021d61573e9a959fb4a1c7a557e9fdfbfae25ed5
eswaryaga-tomtom <eswar.yaga@tomtom.com>
2021-04-16 13:13:07 +0200

    add data

---
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@PostMapping(value = "/data", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String data(HttpEntity<String> httpEntity) {
+		return httpEntity.getBody();
+	}
+}
