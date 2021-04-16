d03eedc42cdc4ebb9ac4207039ee22dd613bb93b
eswaryaga-tomtom <eswar.yaga@tomtom.com>
2021-04-16 15:18:13 +0200

    add data

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -23,7 +23,7 @@ public class GreetingController {
 	}
 
 	@PostMapping(value = "/data", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public JSONObject data(HttpEntity<String> httpEntity) {
+	public String data(HttpEntity<String> httpEntity) {
 
 		System.out.println(httpEntity.getBody());
 		JSONArray jsonObject = new JSONArray(httpEntity.getBody());
@@ -33,6 +33,6 @@ public class GreetingController {
 
 		j2.put("validationResponse",jsonObject.getJSONObject(0).getJSONObject("data").getString("validationCode"));
 
-		return j2;
+		return j2.toString();
 	}
 }
